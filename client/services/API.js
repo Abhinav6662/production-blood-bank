8a55f6707c41838036ca56d7da007d44ea39aca3
Abhinav6662 <abhinav66623@gmail.com>
2025-06-22 18:02:52 +0530

    Update API.js

--- client/services/API.js
+++ client/services/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API=axios.create({baseURL:https://production-blood-bank-1.onrender.com/api/v1,
+const API=axios.create({baseURL:'https://production-blood-bank-1.onrender.com/api/v1'
 })
 
 API.interceptors.request.use((req)=>{
