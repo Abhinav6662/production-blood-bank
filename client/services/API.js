df3d0302fefd7345588ca27d5e8e661bee68fdb2
Abhinav6662 <abhinav66623@gmail.com>
2025-06-22 17:48:18 +0530

    Update API.js

--- client/services/API.js
+++ client/services/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API=axios.create({baseURL:import.meta.env.VITE_URL,
+const API=axios.create({baseURL:https://production-blood-bank-1.onrender.com/api/v1,
 })
 
 API.interceptors.request.use((req)=>{
